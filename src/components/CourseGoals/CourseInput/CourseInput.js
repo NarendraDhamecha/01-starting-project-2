21091ea7517987c60b25cd197392cb777447f029
NarendraDhamecha <nikhildhamecha19@gmail.com>
2023-01-09 09:37:21 +0530

    dynamic_styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,23 +5,30 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setValid] = useState(true);
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
+    setValid(true);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+
+    if(enteredValue.trim().length===0){
+      setValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button valid={isValid} type="submit">Add Goal</Button>
     </form>
   );
 };
